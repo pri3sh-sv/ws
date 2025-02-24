bddafe9dfa665ec1ae5dc28626c76d6f2694559b
pritshSV <swimngo.dev@gmail.com>
2025-02-24 23:48:43 +0530

    go-client code

---
+++ ws-client.go
@@ -0,0 +1,41 @@
+package Hassan_WebSocket
+
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"time"
+)
+
+var url = "ws://localhost:8080/ws"
+
+func connectToWebSocket() {
+	for {
+		log.Println("Connecting to WebSocket...")
+
+		conn, _, err := websocket.DefaultDialer.Dial(url, nil)
+		if err != nil {
+			log.Printf("WebSocket connection error: %v", err)
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		log.Println("WebSocket connected")
+
+		for {
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Printf("WebSocket read error: %v", err)
+				break
+			}
+			log.Println("WebSocket received:", string(message))
+		}
+
+		conn.Close()
+		log.Println("Reconnecting in 5 seconds...")
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func main() {
+	connectToWebSocket()
+}
